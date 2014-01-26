43dc8abc339457947fcb16383286964b29a67737
ArmandoPC <armand1live@gmail.com>
2014-01-26 18:50:54 -0500

    Creacion proyecto Python

---
+++ hola.py
@@ -0,0 +1,17 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Armando"
+__date__ ="$21-ene-2014 7:40:37$"
+
+if __name__ == "__main__":
+    print "Hello World"
+else:
+    print "jejejej"
+
+    
+        
+    
+
+
